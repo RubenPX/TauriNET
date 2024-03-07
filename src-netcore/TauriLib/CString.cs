c66c2455a0681836eb2c5020a0a21f0255adc096
RubenPX <rubenmarchante@gmail.com>
2024-03-07 09:35:21 +0100

    Add .net project

---
+++ src-netcore/TauriLib/CString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TauriIPC; 
+
+public static class CString {
+    private static unsafe delegate*<char*, int, byte*> CopyToCString;
+
+    [UnmanagedCallersOnly]
+    public static unsafe void SetCopyToCStringFunctionPtr(delegate*<char*, int, byte*> copyToCString) => CopyToCString = copyToCString;
+
+    [UnmanagedCallersOnly]
+    public unsafe static byte* GetNameAsCString() {
+        var name = TestApp.Main.getStringFromFile();
+        fixed (char* ptr = name) {
+            return CopyToCString(ptr, name.Length);
+        }
+    }
+}
---
+++ src-netcore/TestApp/Main.cs
@@ -0,0 +1,15 @@
+﻿namespace TestApp {
+    public class Main {
+        public static string getStringFromFile() {
+            var currentPath = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(currentPath, "Test.txt");
+
+            if (!File.Exists(filePath)) {
+                File.Create(filePath).Close();
+                File.WriteAllText(filePath, "This is an example text from this file");
+            }
+
+            return File.ReadAllText(filePath);
+        }
+    }
+}
