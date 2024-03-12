6a859d5a0848479273a5835bad34a1478ca06377
RubenPX <rubenmarchante@gmail.com>
2024-03-12 13:07:54 +0100

    simplify developer experiece coding

--- src/TauriComunication.ts
+++ src/TauriComunication.ts
@@ -19,9 +19,13 @@ export class TauriLib {
     return this.num++;
   }
   
-  public static async invokePlugin<T>(data: Omit<PluginRequest, 'id'>): Promise<RouteResponse<T>> {
+  public static async invokePlugin<T>(data: Omit<PluginRequest, 'id'>): Promise<T | null> {
     let generatedRequest = {...data, id: this.generateID()};
     let invokeResponse = await invoke('plugin_request', { dataStr: JSON.stringify(generatedRequest) }) as string
-    return JSON.parse(invokeResponse) as RouteResponse<T>
+    let jsonResponse = JSON.parse(invokeResponse) as RouteResponse<T>
+
+    if (jsonResponse.error) throw new Error(jsonResponse.error)
+
+    return jsonResponse.data ?? null as T | null;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,15 @@ import { TauriLib } from "./TauriComunication";
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
-async function greet(user: string) {
+async function greet(user: string): Promise<string | null> {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     let userData = {user: user, pass: "Hmm..."}
 
-    let response = await TauriLib.invokePlugin<string>({ plugin: "TestApp", method: "login", data: userData });
-    console.log(response);
-
-    if (response.data) {
-      return response.data;
-    } else if (response.error) {
-      return "ERR: " + response.error;
+    try {
+      return await TauriLib.invokePlugin<string>({ plugin: "TestApp", method: "login", data: userData });
+    } catch (error) {
+      return "ERR: " + error;
     }
-
-    return JSON.stringify(response)
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -24,6 +19,6 @@ window.addEventListener("DOMContentLoaded", () => {
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", async (e) => {
     e.preventDefault();
-    if (greetMsgEl) greetMsgEl.innerHTML = await greet(greetInputEl?.value ?? "");
+    if (greetMsgEl) greetMsgEl.innerHTML = await greet(greetInputEl?.value ?? "") ?? "null";
   });
 });
