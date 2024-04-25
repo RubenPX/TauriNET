0b7d8b36e288aa556ca43cab84eacfff409a327a
RubenPX <rubenmarchante@gmail.com>
2024-04-25 15:39:01 +0200

    More descriptive exception

--- src-netcore/TauriComunication/Utils.cs
+++ src-netcore/TauriComunication/Utils.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace TauriComunication {
 	public class Utils {
 		public static T ParseObject<T>(object data) {
-			if (data == null) throw new ArgumentNullException("data");
+			if (data == null) throw new ArgumentNullException("parameter data is null");
 			return ((JObject)data).ToObject<T>()!;
 		}
 	}
